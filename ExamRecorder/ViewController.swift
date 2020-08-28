088c6fc17e1019f9c6d436d9ebdc15fcebaa69f2
Sayaka Abe <sayaka.abe1207@gmail.com>
2020-08-28 15:39:42 +0900

    Initial Commit

---
+++ ExamRecorder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExamRecorder
+//
+//  Created by Sayaka Abe on 2020/08/28.
+//  Copyright © 2020 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ExamRecorderTests/ExamRecorderTests.swift
@@ -0,0 +1,34 @@
+//
+//  ExamRecorderTests.swift
+//  ExamRecorderTests
+//
+//  Created by Sayaka Abe on 2020/08/28.
+//  Copyright © 2020 com.litech. All rights reserved.
+//
+
+import XCTest
+@testable import ExamRecorder
+
+class ExamRecorderTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
